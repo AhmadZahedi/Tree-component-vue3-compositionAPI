3356336bfd8cc56056df24acaccd5544f481660f
Ahmad Zahedi <ahmad.zahedi121288@gmail.com>
2023-10-24 21:30:50 +0330

    style(TreeNodeComponent): add some styles

--- src/components/TreeNodeComponent.vue
+++ src/components/TreeNodeComponent.vue
@@ -1,7 +1,8 @@
 <template>
-  <li>
-    <div class="d-flex">
-      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none" viewBox="0 0 24 24">
+  <li class="mt-2">
+    <div class="d-flex gap-2 align-items-center">
+      <svg @click="toggleChildrenList(node)" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none"
+           viewBox="0 0 24 24">
         <path fill="#00e1ff" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" opacity=".1"/>
         <path stroke="#00e1ff" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
         <path stroke="#00e1ff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -12,25 +13,35 @@
           type="checkbox"
           :id="node[itemValue]"
           :checked="node.__selected"
+          class="form-check-input me-1 custom-check"
       >
 
-      <label :for="node[itemValue]">
+      <label
+          :for="node[itemValue]"
+          class="form-check-label"
+      >
         {{ node[itemTitle] }}
       </label>
     </div>
-  </li>
 
-  <ul v-if="node.children">
-    <TreeNodeComponent
-        v-for="child in node.children"
-        :node="child"
-        :item-title="itemTitle"
-        :item-value="itemValue"
-    />
-  </ul>
+    <ul
+        v-if="node.children"
+        ref="childrenListElement"
+        :style="childrenListStyles"
+    >
+      <TreeNodeComponent
+          v-for="child in node.children"
+          :node="child"
+          :item-title="itemTitle"
+          :item-value="itemValue"
+      />
+    </ul>
+  </li>
 </template>
 
 <script>
+import {computed, ref} from "vue";
+
 export default {
   props: {
     node: {
@@ -46,8 +57,28 @@ export default {
       required: true
     }
   },
-  setup() {
-    return {}
+  setup(props) {
+    const childrenListElement = ref(null);
+
+    const childrenListHeight = computed(() => {
+      return childrenListElement.value?.clientHeight;
+    });
+
+    const childrenListStyles = computed(() => {
+      return [
+        props.node.__expanded ? 'height: 0' : `height: ${childrenListHeight}px`,
+        'overflow: hidden'
+      ];
+    });
+
+    function toggleChildrenList(targetNode) {
+      targetNode.__expanded = !targetNode.__expanded;
+    }
+
+    return {
+      childrenListStyles,
+      toggleChildrenList
+    };
   }
 }
 </script>
@@ -55,5 +86,11 @@ export default {
 <style scoped>
 li {
   list-style: none;
+}
+
+.custom-check {
+  width: 25px;
+  height: 25px;
+  margin-top: 0 !important;
 }
 </style>

1f2ce11188f6c38d27428dbabe822be021c07f70
Ahmad Zahedi <ahmad.zahedi121288@gmail.com>
2023-10-24 20:32:23 +0330

    feat(TreeComponent): add props

--- src/components/TreeComponent.vue
+++ src/components/TreeComponent.vue
@@ -1,13 +1,34 @@
 <template>
 Tree component
-<TreeNode />
+<TreeNode
+    v-for="node in data"
+    :node="node"
+    :itemValue="itemValue"
+    :itemTitle="itemTitle"
+/>
 </template>
 
 <script>
-import TreeNode from "./TreeNode.vue";
+import TreeNode from "./TreeNodeComponent.vue";
 export default {
-  components: {TreeNode},
-  setup() {
+  components: { TreeNode },
+  props: {
+    data: {
+      type: Array,
+      required: true
+    },
+    itemValue: {
+      type: String,
+      default: 'value'
+    },
+    itemTitle: {
+      type: String,
+      default: 'title'
+    }
+  },
+  setup(props) {
+    console.log(props.data)
+
     return {}
   }
 }

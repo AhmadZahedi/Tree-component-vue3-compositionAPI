6f191c46ec63a32f0fa9ab3e1269c5963a82d2a6
Ahmad Zahedi <ahmad.zahedi121288@gmail.com>
2023-11-11 20:33:28 +0330

    refactor(App): change usage according to new structure

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,12 @@
   <main>
     <div class="vw-100 d-flex justify-content-center">
       <TreeComponent
-          :rawData="rawData"
+          v-model="result"
+          :raw-data="data"
           item-title="label"
           item-value="id"
+          :dependent="false"
+          @update:modelValue="finish"
       />
     </div>
   </main>
@@ -16,26 +19,33 @@
 
 <script>
 import TreeComponent from "./components/TreeComponent.vue";
+import {ref} from "vue";
 
 export default {
   components: {
     TreeComponent
   },
   setup() {
-    const rawData = [];
+    const data = [];
 
     for (let i = 1; i <= 100; i++) {
       const parent_id = i > 1 ? Math.floor(Math.random() * (i - 1)) + 1 : null;
       const label = `Item ${i}`;
       const value = `item_${i}`;
 
-      rawData.push({id: i, parent_id, label, value});
+      data.push({id: i, parent_id, label, value});
     }
 
+    const result = ref([1, 3, 2, 8]);
 
+    function finish(outputArr) {
+      console.log('finished ', outputArr);
+    }
 
     return {
-      rawData
+      data,
+      result,
+      finish
     };
   }
 }
